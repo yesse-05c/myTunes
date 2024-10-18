c7e640ac81a1d0c4fe9047feaf345514d88b8dca
Yesenia Callejas <mcallejas@harding.edu>
2024-10-18 16:36:13 -0500

    myTunes project creation.
    ListBox displays playlists, missing to display All Music.
    DataGrid schema created.

---
+++ myTunes/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace myTunes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MusicRepo musicRepo;
+        public MainWindow()
+        {
+            InitializeComponent();
+            musicRepo = new MusicRepo();
+            string[] playlists = musicRepo.Playlists;
+            songsListBox.ItemsSource = playlists;
+        }
+    }
+}
