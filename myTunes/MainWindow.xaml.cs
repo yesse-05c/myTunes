ee5d3d907463455e301a3794d29c7ebc86c9717f
Yesenia Callejas <mcallejas@harding.edu>
2024-10-23 19:05:52 -0500

    Play and Stop commands added to xaml. Feature not working yet.

--- myTunes/MainWindow.xaml.cs
+++ myTunes/MainWindow.xaml.cs
@@ -15,14 +15,29 @@ namespace myTunes
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public static class CustomCommands
+    {
+        // Make command accessible to XAML
+        public static readonly RoutedUICommand Play = new RoutedUICommand(
+                 "Play", "Play", typeof(CustomCommands));
+        public static readonly RoutedUICommand Stop = new RoutedUICommand(
+            "Stop", "Stop", typeof(CustomCommands));
+    }
+
     public partial class MainWindow : Window
     {
         private readonly MusicRepo musicRepo;
+        private readonly MediaPlayer mediaPlayer;
+        private bool isPlaying = false;
+
+
 
         public MainWindow()
         {
             InitializeComponent();
             musicRepo = new MusicRepo();
+            mediaPlayer = new MediaPlayer();
             var playlists = musicRepo.Playlists;
             playlists.Insert(0, "All Music");
             //Bind playlists
@@ -31,5 +46,26 @@ namespace myTunes
             dataGrid.ItemsSource = musicRepo.Songs.DefaultView;
             
         }
-    } 
+        private void PlayCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+            isPlaying = true;
+        }
+
+        private void StopCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+            isPlaying = false;
+        }
+        private void StopCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isPlaying;
+        }
+
+        private void PlayCommand_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = dataGrid.ItemsSource != null;
+        }
+
+    }
 }
